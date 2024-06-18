296f43d47accf5cd9fce28e50d48ebe59c489a9f
Rogério Yokoi <rogerio.yokoi@hotmail.com>
2024-06-18 16:49:51 -0300

    feat: tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  darkMode: ['selector', '[data-mode="dark"]'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
