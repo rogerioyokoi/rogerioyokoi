21a4c0d0b4ab3abd4ef0b7b5839cc4d9a2f5f10a
Rogério Yokoi <rogerio.yokoi@hotmail.com>
2024-06-27 12:46:11 -0300

    fix: fix HeaderApplication test

--- src/components/organisms/headerApplication/headerApplication.test.tsx
+++ src/components/organisms/headerApplication/headerApplication.test.tsx
@@ -1,12 +1,16 @@
 import { render } from '@testing-library/react';
-import { describe, it } from 'node:test';
-import { expect } from 'vitest';
+import { MemoryRouter } from 'react-router-dom';
+import { describe, expect, it } from 'vitest';
 import { axe } from 'vitest-axe';
 import HeaderApplication from './headerApplication';
 
 describe('Components > Organisms > HeaderApplication', () => {
   it('Should render without accessibility violations', async () => {
-    const { container } = render(<HeaderApplication />);
+    const { container } = render(
+      <MemoryRouter>
+        <HeaderApplication />
+      </MemoryRouter>
+    );
 
     expect(await axe(container)).toHaveNoViolations();
   });
