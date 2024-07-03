ac8c198df01b3a1a665f924d7d1d05fed0837f38
Rogério Yokoi <rogerio.yokoi@hotmail.com>
2024-07-03 15:34:06 -0300

    test: update Home snapshot test"

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,3 +1,4 @@
+import { PaperAirplaneIcon } from '@heroicons/react/24/solid';
 import { FC, useMemo } from 'react';
 import { baseDescription } from '../../../assets/data/meta.const';
 import Avatar from '../../atoms/avatar/avatar';
@@ -5,6 +6,12 @@ import Title from '../../atoms/title/title';
 
 import { useMediaQuery } from '@react-hook/media-query';
 import Photo from '../../../assets/pessoal.jpg';
+
+import Button from '../../atoms/button/button';
+import Icon from '../../atoms/icon/icon';
+
+import { ROUTES_NAV_IDS } from '@/routes/routes';
+import LinkedINIcon from '@assets/svg/linkedIn.svg?react';
 
 const Home: FC = () => {
   const smallSizePhoto = useMediaQuery('(max-width: 1279px)');
@@ -17,13 +24,34 @@ const Home: FC = () => {
   }, [smallSizePhoto]);
 
   return (
-    <div className="flex flex-col-reverse xl:flex-row gap-6 w-100 items-center">
+    <div className="flex flex-col-reverse xl:flex-row gap-12 w-100 items-center">
       <div className="w-100 xl:w-2/4 flex justify-center">
         <Avatar src={Photo} alt="Foto de rosto do proprietário do website: Rogério Yokoi" size={getAvatarSize} />
       </div>
-      <div className="w-100 xl:w-2/4 flex flex-col gap-6">
+      <div className="w-100 xl:w-2/4 flex flex-col gap-12">
         <Title title="Olá, eu sou Rogério Yokoi" subTitle="Engenheiro de software Front-End" />
+
         <p data-testid="home-description">{baseDescription}</p>
+
+        <div className="flex w-full justify-center gap-12">
+          <Button
+            href="https://www.linkedin.com/in/rogerioyokoi/"
+            target="new"
+            color="secondary"
+            endIcon={<Icon style={{ backgroundColor: '#0077b5' }} Component={<LinkedINIcon />} />}
+          >
+            {' '}
+            Linked IN{' '}
+          </Button>
+
+          <Button
+            href={ROUTES_NAV_IDS.ABOUT}
+            color="primary"
+            endIcon={<Icon className="bg-amber-500" Component={<PaperAirplaneIcon />} />}
+          >
+            Mais sobre mim
+          </Button>
+        </div>
       </div>
     </div>
   );
