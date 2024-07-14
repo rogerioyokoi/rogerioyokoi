8efc94f60608d374613d11ee50f4ecbf12e0e79c
Rogério Yokoi <rogerio.yokoi@hotmail.com>
2024-07-14 18:46:02 -0300

    feat: add Metatags on home page

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -3,6 +3,7 @@ import { baseDescription } from '../../../assets/data/meta.const';
 import Avatar from '../../atoms/avatar/avatar';
 import Title from '../../atoms/title/title';
 
+import MetaTags from '@/components/atoms/metaTags/metaTags';
 import PersonalInfoLInks from '@/components/organisms/personalInfoLinks/personalInfoLinks';
 import { useMediaQuery } from '@react-hook/media-query';
 import Photo from '../../../assets/pessoal.jpg';
@@ -18,18 +19,22 @@ const Home: FC = () => {
   }, [smallSizePhoto]);
 
   return (
-    <div className="flex flex-col-reverse xl:flex-row gap-12 w-100 items-center">
-      <div className="w-100 xl:w-2/4 flex justify-center">
-        <Avatar src={Photo} alt="Foto de rosto do proprietário do website: Rogério Yokoi" size={getAvatarSize} />
-      </div>
-      <div className="w-100 xl:w-2/4 flex flex-col gap-12">
-        <Title title="Olá, eu sou Rogério Yokoi" subTitle="Engenheiro de software Front-End" />
+    <>
+      <MetaTags page={'home'} />
 
-        <p data-testid="home-description">{baseDescription}</p>
+      <div className="flex flex-col-reverse xl:flex-row gap-12 w-100 items-center">
+        <div className="w-100 xl:w-2/4 flex justify-center">
+          <Avatar src={Photo} alt="Foto de rosto do proprietário do website: Rogério Yokoi" size={getAvatarSize} />
+        </div>
+        <div className="w-100 xl:w-2/4 flex flex-col gap-12">
+          <Title title="Olá, eu sou Rogério Yokoi" subTitle="Engenheiro de software Front-End" />
 
-        <PersonalInfoLInks />
+          <p data-testid="home-description">{baseDescription}</p>
+
+          <PersonalInfoLInks />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
